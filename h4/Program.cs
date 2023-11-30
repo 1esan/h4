a83c808bd267bfeef9b182bbf943fc0824464c54
1esan <egor.labutin01@yandex.ru>
2023-11-30 11:27:26 +0300

    последнее обновление

---
+++ h4/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите количество строк массива:");
+        int n = int.Parse(Console.ReadLine()); 
+        Console.WriteLine("Введите количество столбцов массива:");
+        int m = int.Parse(Console.ReadLine()); 
+        int[,] array = new int[n, m]; 
+        Console.WriteLine("Введите элементы массива построчно, разделяя пробелами:");
+        for (int i = 0; i < n; i++) 
+        {
+            string[] line = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries); 
+            for (int j = 0; j < m; j++) 
+            {
+                if (int.TryParse(line[j], out int value))
+                {
+                    array[i, j] = value;
+                }
+                else
+                {
+                    array[i, j] = 0;
+                }
+            }
+        }
+
+        
+        Console.WriteLine("Введенный массив:");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        
+        Console.WriteLine("Для каждой строки массива находим минимум, максимум и сумму:");
+        for (int i = 0; i < n; i++)
+        {
+            FindMin(array, i, out int min); 
+            FindMax(array, i, out int max); 
+            FindSum(array, i, out int sum); 
+            Console.WriteLine($"Строка {i}: минимум = {min}, максимум = {max}, сумма = {sum}"); 
+        }
+    }
+    static void FindMin(in int[,] array, int row, out int min)
+    {
+        min = int.MaxValue; 
+        for (int j = 0; j < array.GetLength(1); j++) 
+        {
+            if (array[row, j] < min) 
+            {
+                min = array[row, j]; 
+            }
+        }
+    }
+    static void FindMax(in int[,] array, int row, out int max)
+    {
+        max = int.MinValue; 
+        for (int j = 0; j < array.GetLength(1); j++) 
+        {
+            if (array[row, j] > max) 
+            {
+                max = array[row, j]; 
+            }
+        }
+    }
+
+    static void FindSum(in int[,] array, int row, out int sum)
+    {
+        sum = 0; 
+        for (int j = 0; j < array.GetLength(1); j++) 
+        {
+            sum += array[row, j]; 
+        }
+    }
+}
---
+++ task2/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+class IntArray
+{
+    
+    private int[] data;
+
+    
+    public IntArray(int size)
+    {
+        if (size < 0)
+        {
+            throw new ArgumentException("Размер массива не может быть отрицательным");
+        }
+        data = new int[size];
+    }
+
+    
+    public void InputData()
+    {
+        for (int i = 0; i < data.Length; i++)
+        {
+            Console.Write("Введите элемент {0}: ", i);
+            data[i] = int.Parse(Console.ReadLine());
+        }
+    }
+    public void InputDataRandom()
+    {
+        Random random = new Random();
+        for (int i = 0; i < data.Length; i++)
+        {
+            data[i] = random.Next(-100, 100);
+        }
+    }
+
+    public void Print(int start, int end)
+    {
+        if (start < 0 || start >= data.Length || end < 0 || end >= data.Length)
+        {
+            throw new IndexOutOfRangeException("Индекс вне диапозона размерности массива");
+        }
+        if (start > end)
+        {
+            throw new ArgumentException("Начальный индекс должен быть меньше или равен конечному индексу.");
+        }
+        Console.Write("Массив от {0} до {1}: ", start, end);
+        for (int i = start; i <= end; i++)
+        {
+            Console.Write(data[i] + " ");
+        }
+        Console.WriteLine();
+    }
+
+    public void FindValue(int value, out int[] indexes)
+    {
+        indexes = new int[0];
+        int count = 0;
+        for (int i = 0; i < data.Length; i++)
+        {
+            if (data[i] == value)
+            {
+                Array.Resize(ref indexes, count + 1);
+                indexes[count] = i;
+                count++;
+            }
+        }
+    }
+
+    public void DelValue(ref int value)
+    {
+        FindValue(value, out int[] indexes);
+        if (indexes.Length == 0)
+        {
+            return;
+        }
+        int[] newData = new int[data.Length - indexes.Length];
+        int j = 0; 
+        for (int i = 0; i < data.Length; i++)
+        {
+            if (data[i] != value)
+            {
+                newData[j] = data[i];
+                j++;
+            }
+        }
+        data = newData;
+        value = 0;
+    }
+
+    public static int FindMax(in IntArray array)
+    {
+        if (array.data.Length == 0)
+        {
+            throw new InvalidOperationException("Массив пуст");
+        }
+        int max = array.data[0];
+        for (int i = 1; i < array.data.Length; i++)
+        {
+            if (array.data[i] > max)
+            {
+                max = array.data[i];
+            }
+        }
+        return max;
+    }
+
+    public static IntArray Add(in IntArray array1,in IntArray array2)
+    {
+        if (array1.data.Length != array2.data.Length)
+        {
+            throw new ArgumentException("Массивы должны иметь одинаковую длину");
+        }
+        IntArray result = new IntArray(array1.data.Length);
+        for (int i = 0; i < result.data.Length; i++)
+        {
+            result.data[i] = array1.data[i] + array2.data[i];
+        }
+        return result;
+    }
+
+    public void Sort()
+    {
+        for (int i = 0; i < data.Length - 1; i++)
+        {
+            for (int j = 0; j < data.Length - i - 1; j++)
+            {
+                if (data[j] > data[j + 1])
+                {
+                    int temp = data[j];
+                    data[j] = data[j + 1];
+                    data[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
